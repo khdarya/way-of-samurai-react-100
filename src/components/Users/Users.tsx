9196cfe63f68daba2c4b65e7b89c648659d35263
khdarya <darya.khakhlova@gmail.com>
2020-09-16 14:55:08 -0400

    users added 49

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export type UsersPropsType = {}
+
+let Users = (props: UsersPropsType) => {
+    return (
+        <div>
+            users will be here
+        </div>
+    )
+}
+
+export default Users;
