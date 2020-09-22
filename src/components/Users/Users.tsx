17e3c8ed9dd2064146f1a2bde6230b65f0d04bdf
khdarya <darya.khakhlova@gmail.com>
2020-09-22 10:16:21 -0400

    52 side effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -15,14 +15,19 @@ let Users = (props: UsersPropsType) => {
 
     const {users, followUsers, unfollowUsers, setUsers} = props;
 
-    if (users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            setUsers(response.data.items)
-        })
+    let getUsers = () => {
+
+
+        if (users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}> Get Users</button>
             {
                 users.map(u => <div key={u.id}>
                     <span>
