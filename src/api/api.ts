700521ae75d12260772d87ebea39724bd90cfa31
khdarya <darya.khakhlova@gmail.com>
2020-10-10 11:12:32 -0400

    63

---
+++ src/api/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "979d9ce7-5a57-44f9-9b9c-37da1881885c"
+    }
+});
+
+export const usersAPI = {
+    getUsers (currentPage: number, pageSize: number) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,).then(response => {
+            return response.data;
+        });
+    }
+}
+
+
+
+export const getUsers2 = (currentPage: number, pageSize: number) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`,).then(response => {
+            return response.data;
+    })
+}
+
